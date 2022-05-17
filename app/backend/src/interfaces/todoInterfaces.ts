17874c9be825fc025eeb14f501990fcd9b2500c1
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-17 16:59:48 -0400

    :sparkles: Feat(update and delete): RoutesPATCH and DELETE "/todos/:id" ... Description: Created methods updateTodo and deleteTodo in routes PATCH and DELETE "/todos/:id". Also created interfaces to contemplate its methods.

--- app/backend/src/interfaces/todoInterfaces.ts
+++ app/backend/src/interfaces/todoInterfaces.ts
@@ -23,10 +23,18 @@ export enum SortBy {
   status = 'status',
 }
 
+export enum PropertyToEdit {
+  subject = 'subject',
+  description = 'description',
+  status = 'status',
+}
+
 export interface ITodoController {
   getAllTodos(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   getTodosByStatus(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   insertNewTodo(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  updateTodo(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  deleteTodo(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
 }
 
 export interface ITodoService {
@@ -34,4 +42,6 @@ export interface ITodoService {
   getSortedTodos(valueToSort: string): Promise<ITodo[]>;
   getTodosByStatus(status: string): Promise<ITodo[]>;
   insertNewTodo(newTodo: ITodo): Promise<ITodo>;
+  updateTodo(id: number, propertyToEdit: string, newPropertyInfo: string): Promise<boolean | void>;
+  deleteTodo(id: number): Promise<boolean | void>;
 }
