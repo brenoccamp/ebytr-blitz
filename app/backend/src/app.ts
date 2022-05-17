226b422842aabc9518bf84c0ccf030b933bc72f3
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-17 12:03:03 -0400

    :sparkles: Feat(app.ts): Routes GET/POST and erros middlewares ... Description: Created routes GET ("/todos?sortBy" and "/todos/:status") and POST ("/todos") to list all todos, sort todos by query param and insert new todo. Also implemented this.app to use erros middleware.

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,9 +1,20 @@
 import * as express from 'express';
+import TodoController from './api/controllers/todoController';
+import TodoService from './api/services/todoService';
+import { ITodoController, ITodoService } from './interfaces/todoInterfaces';
+import ErrorMiddleware from './api/middlewares/error';
 
 class App {
   public app: express.Express;
 
+  private _todoController: ITodoController;
+
+  private _todoService: ITodoService;
+
   constructor() {
+    this._todoService = new TodoService();
+    this._todoController = new TodoController(this._todoService);
+
     this.app = express();
     this.app.use(express.json());
 
@@ -19,6 +30,36 @@ class App {
     };
 
     this.app.use(accessControl);
+    this.todoRoutes();
+
+    this.app.use(ErrorMiddleware);
+  }
+
+  private todoRoutes(): void {
+    this.app.get(
+      '/todos',
+      this._todoController.getAllTodos,
+    );
+
+    this.app.get(
+      '/todos/:status',
+      this._todoController.getTodosByStatus,
+    );
+
+    this.app.post(
+      '/todos',
+      this._todoController.insertNewTodo,
+    );
+
+    this.app.patch(
+      '/todos/:id',
+      /* todos controller */
+    );
+
+    this.app.delete(
+      '/todos/:id',
+      /* todos controller */
+    );
   }
 
   public start(PORT: number): void {
