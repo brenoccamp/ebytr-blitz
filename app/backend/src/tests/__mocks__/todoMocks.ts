5e8759cb4012b6da4ca04a0085b89841c281f79d
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-18 13:41:54 -0400

    :white_check_mark: Test(I&T): Tested route GET "/todos?sortBy" ... Description: Created Integration Testing of route GET "/todos?sortBy". Also add a script to package.json to run all tests and created mocks of functions getAllTodos and getSortedTodos.

---
+++ app/backend/src/tests/__mocks__/todoMocks.ts
@@ -0,0 +1,104 @@
+export const allTodos = [
+  {
+    id: 1,
+    subject: "Task 1 to test user1",
+    description: "Task pending added for test seeders and other behaviors",
+    status: "pending",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "Task in progress added for test seeders and other behaviors",
+    status: "in progress",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+];
+
+export const toodsSortedBySubject = [
+  {
+    id: 1,
+    subject: "Task 5 to test user1",
+    description: "Task pending added for test seeders and other behaviors",
+    status: "pending",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+  {
+    id: 2,
+    subject: "Task 6 to test user1",
+    description: "Task in progress added for test seeders and other behaviors",
+    status: "in progress",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+];
+
+export const toodsSortedByDescription = [
+  {
+    id: 1,
+    subject: "Task 1 to test user1",
+    description: "1Task pending added for test seeders and other behaviors",
+    status: "pending",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "2Task in progress added for test seeders and other behaviors",
+    status: "in progress",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+];
+
+export const toodsSortedByCreatedAt = [
+  {
+    id: 1,
+    subject: "Task 1 to test user1",
+    description: "1Task pending added for test seeders and other behaviors",
+    status: "pending",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "2Task in progress added for test seeders and other behaviors",
+    status: "in progress",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.532Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+];
+
+export const toodsSortedByStatus = [
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "2Task in progress added for test seeders and other behaviors",
+    status: "in progress",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.532Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+  {
+    id: 1,
+    subject: "Task 1 to test user1",
+    description: "1Task pending added for test seeders and other behaviors",
+    status: "pending",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.421Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  },
+];
