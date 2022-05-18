61f5302753ab97d22fbcc781cc9baaa1314eb290
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-18 15:03:10 -0400

    :white_check_mark: Tests(I&T): POST, PATCH, DELETE ... Description: Created Integration Testing of routes: POST ("/todos"), PATCH ("/todos/:id"), DELETE ("/todos/:id") and its necessary mocks.

--- app/backend/src/tests/__mocks__/todoMocks.ts
+++ app/backend/src/tests/__mocks__/todoMocks.ts
@@ -102,3 +102,62 @@ export const toodsSortedByStatus = [
     updatedAt: "2022-05-18T15:56:33.421Z"
   },
 ];
+
+export const doneTodos = [
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "2Task in progress added for test seeders and other behaviors",
+    status: "done",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.532Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  }
+];
+
+export const inProgressTodos = [
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "2Task in progress added for test seeders and other behaviors",
+    status: "inProgress",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.532Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  }
+];
+
+export const pendingTodos = [
+  {
+    id: 2,
+    subject: "Task 2 to test user1",
+    description: "2Task in progress added for test seeders and other behaviors",
+    status: "pending",
+    userId: 1,
+    createdAt: "2022-05-18T15:56:33.532Z",
+    updatedAt: "2022-05-18T15:56:33.421Z"
+  }
+];
+
+export const insertedTodo = {
+  "id": 10,
+  "subject": "Teste2",
+  "description": "Testing insert new todo",
+  "status": "pending",
+  "userId": 1,
+  "updatedAt": "2022-05-18T18:05:24.112Z",
+  "createdAt": "2022-05-18T18:05:24.112Z"
+}
+
+export const incompleteBody = `Body must have followed properties: "subject",
+              "description", "status" and "user id"`
+
+export const todoByPk = {
+  "id": 1,
+  "subject": "Task 1 to test user1",
+  "description": "Task pending added for test seeders and other behaviors",
+  "status": "pending",
+  "userId": "1",
+  "createdAt": "2022-05-18T18:22:57.000Z",
+  "updatedAt": "2022-05-18T18:22:57.000Z"
+}
